9dd0b5e1090bd52335f70079fe12c5c59dcf12a0
Divre4Tnk <ahmadfahtoni21@gmail.com>
2025-10-07 13:08:59 +0700

    pisah slug lokasi

--- database/migrations/2025_10_07_130822_add_slug_to_locations_table.php
+++ database/migrations/2025_10_07_130822_add_slug_to_locations_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('locations', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('locations', function (Blueprint $table) {
+            $table->string('slug');
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('locations');
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
     }
 };
